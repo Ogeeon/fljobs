9ad782c897339e4f472501ec974bdaf067165aec
Alexej Bogatyrev <avbogatyrev@gmail.com>
2017-08-27 17:14:18 +1000

    Test

--- dbconnect.php
+++ dbconnect.php
@@ -2,7 +2,8 @@
 
 // доступ к базе на OpenShift
 
-$dbhost = getenv('fljmysql');
+$dbhost = getenv('fljmysql_SERVICE_HOST');
+echo 'dbhost='.$dbhost."/n<br>";
 $dbname = 'fljobs';
 $dbuser = 'admin3p6KSFX';
 $dbpass = '8VIZ1trAVlBa';
